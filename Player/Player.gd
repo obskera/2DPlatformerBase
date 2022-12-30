a51ebbf6e24cd09d12c5dce60b9d82b976c81f81
Obskera <vivifydecepi@gmail.com>
2022-12-29 22:24:44 -0800

    door transition added

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 class_name Player
+#BUGS: 
+##1. Double jump is triple jump? 
+##2. Can't jump while standing in front of flag. Possible collision/is colliding thing?
 
 enum{ MOVE, CLIMB }
 
 #allows you to adjust numbers in editor; casts for autocomplete
-export(Resource) var moveData = preload("res://FastPlayerMovementData.tres") as PlayerMovementData
+export(Resource) var moveData = preload("res://Player/FastPlayerMovementData.tres") as PlayerMovementData
 
 #Velocity
 var velocity : Vector2 = Vector2.ZERO
@@ -26,7 +29,7 @@ onready var remoteTransform2D = $RemoteTransform2D
 func _ready():
 	#cast for autocompletion
 	## moveData = moveData as PlayerMovementData
-	animatedSprite.frames = load ("res://PlayerGreenSkin.tres")
+	animatedSprite.frames = load ("res://Player/PlayerGreenSkin.tres")
 #default fps is 60fps, so delta is 1/60th
 func _physics_process(delta):
 	var input = Vector2.ZERO
@@ -36,8 +39,8 @@ func _physics_process(delta):
 	input.y = Input.get_axis("ui_up", "ui_down")
 	#match statement is like a switch case
 	match state:
-		MOVE: move_state(input)
-		CLIMB: climb_state(input)
+		MOVE: move_state(input, delta)
+		CLIMB: climb_state(input, delta)
 	
 func is_on_ladder():
 	if not ladderCheck.is_colliding(): return false
@@ -45,23 +48,26 @@ func is_on_ladder():
 	if not collider is Ladder: return false
 	return true
 
-func move_state(input):
+func move_state(input, delta):
 	if is_on_ladder() and Input.is_action_just_pressed("ui_up"):
 		state = CLIMB
 	
-	apply_gravity()
+	apply_gravity(delta)
 	if input.x == 0:
-		apply_friction()
+		apply_friction(delta)
 		animatedSprite.animation = "Idle"
 	else:
-		apply_acceleration(input.x)
+		apply_acceleration(input.x, delta)
 		animatedSprite.animation = "Run"
 		#$AnimatedSprite.flip_h = true
+		
+		
 	if not is_on_floor() and Input.is_action_just_pressed("ui_up"): 
 		if jumps_available > 0:
 			jump_sound()
 			velocity.y = moveData.JUMP_FORCE
 			jumps_available -= 1
+	#	else: fast_fall(delta)
 	if is_on_floor() or coyote_jump:
 		if Input.is_action_just_pressed("ui_up") and not ladderCheck.is_colliding():
 			if jumps_available > 1:
@@ -72,7 +78,8 @@ func move_state(input):
 			if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
 				velocity.y = moveData.JUMP_RELEASE_FORCE
 			if velocity.y > 5:
-				velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
+				velocity.y += moveData.ADDITIONAL_FALL_GRAVITY * delta
+				
 		
 	if Input.is_action_pressed("ui_up") and not is_on_floor():
 		buffered_jump = true
@@ -104,7 +111,7 @@ func move_state(input):
 		coyote_jump = true
 		coyoteJumpTimer.start()
 		
-func climb_state(input):
+func climb_state(input, delta):
 	if not is_on_ladder():
 		state = MOVE
 	if input.length() != 0:
@@ -133,16 +140,21 @@ func player_die():
 	#connected in World.gd script:
 	Events.emit_signal("player_died")
 
-func apply_gravity():
-		velocity.y += moveData.GRAVITY
+func apply_gravity(delta):
+		velocity.y += moveData.GRAVITY * delta
 		velocity.y = min(velocity.y, 300)
 
-func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
+func fast_fall(delta):
+	pass
+	#if velocity.y > 0:
+		#velocity.y += moveData.ADDITIONAL_FALL_GRAVITY * delta
+	
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION * delta)
 
-func apply_acceleration(amount):
+func apply_acceleration(amount, delta):
 	animatedSprite.flip_h = velocity.x > 0
-	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION * delta)
 
 func _on_CoyoteJumpTimer_timeout():
 	coyote_jump = false
